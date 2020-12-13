7a24ada5d88d59118d8aa711457a9cc947c7bbbb
Chinedu Samuel <samuelchinedufranklin@gmail.com>
2020-12-13 14:49:38 +0100

    created audience.js router

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const bodyParser = require("body-parser"); // body-parser middleware
 // Routes
 const users = require("./routes/users");
 const emailTemplate = require("./routes/emailTemplates");
+const audience = require("./routes/audience")
 
 const suggestions = require("./controllers/suggestions"); //Suggestions Controller
 const reply = require("./controllers/reply"); //Reply Controller
 const billing_info = require("./controllers/billing_info"); //Reply Controller
 const contactUs = require("./controllers/contactUsController"); // contactUsController module
 const newsletter = require("./controllers/newsletterController"); // newsletterController module
-const audience = require("./controllers/audienceController"); // audienceController module
 const payment = require("./controllers/payment"); // payment module
 const stickyNote = require("./controllers/sticky_note"); // stickyNote module
 const subscriber_group = require("./controllers/subscriber_group"); //subscriber_group module
@@ -22,13 +22,12 @@ app.use(bodyParser.json()); // Middleware use with express
 
 app.use("/api/users", users);
 app.use("/api/emailTemplates", emailTemplate);
-
+app.use("/api/audience", audience);
 suggestions(app);
 reply(app);
 billing_info(app);
 contactUs(app);
 newsletter(app);
-audience(app);
 payment(app);
 stickyNote(app);
 subscriber_group(app);
